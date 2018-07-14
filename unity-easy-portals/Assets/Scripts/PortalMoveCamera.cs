b44c3dc129932aa2de1e23929ddd56a91896ecc5
williambl <william@williambl.com>
2018-07-14 23:37:44 +0100

    Added a script to move and rotate the portal cameras

---
+++ unity-easy-portals/Assets/Scripts/PortalMoveCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMoveCamera : MonoBehaviour {
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 cameraOffset = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + cameraOffset;
+
+        float portalAngleDifference = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(portalAngleDifference, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
