4a43b88a57497233693231d7c6e4dc77646a2e42
williambl <william@williambl.com>
2018-07-15 00:02:03 +0100

    Improved rendering of portals

---
+++ unity-easy-portals/Assets/Scripts/PortalTextureSetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureResizer : MonoBehaviour {
+
+    public Camera cameraA;
+    public Material matA;
+
+    public Camera cameraB;
+    public Material matB;
+
+    // Use this for initialization
+    void Start () {
+        if (cameraA.targetTexture != null)
+            cameraA.targetTexture.Release();
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        matA.mainTexture = cameraA.targetTexture;
+        if (cameraB.targetTexture != null)
+            cameraB.targetTexture.Release();
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        matB.mainTexture = cameraB.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
