e6e6c1ed1509f90a31a0e3ecb827cba23fb5012b
williambl <william@williambl.com>
2018-07-16 16:48:58 +0100

    Added teleportation between portals

---
+++ unity-easy-portals/Assets/Scripts/PortalTeleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour {
+
+    public Transform reciever;
+
+    private List<Transform> overlappingTransforms = new List<Transform>();
+
+    void Update () {
+        if (overlappingTransforms.Count > 0)
+        {
+            List<Transform> forDeletion = new List<Transform>();
+            foreach (Transform overlappingTransform in overlappingTransforms) {
+                Vector3 portalToTarget = overlappingTransform.position - transform.position;
+                float dotProduct = Vector3.Dot(transform.up, portalToTarget);
+
+                if (dotProduct < 0f)
+                {
+                    float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                    overlappingTransform.Rotate(Vector3.up, rotationDiff);
+
+                    Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToTarget;
+                    overlappingTransform.position = reciever.position + positionOffset;
+
+                    forDeletion.Add(overlappingTransform);
+                }
+            }
+            foreach (Transform toDelete in forDeletion)
+                overlappingTransforms.Remove(toDelete);
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (!overlappingTransforms.Contains(other.transform))
+            overlappingTransforms.Add(other.transform);
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        overlappingTransforms.Remove(other.transform);
+    }
+}
