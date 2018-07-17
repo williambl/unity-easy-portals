907d9b89ca885aaa5bd935104d7ac016de548a75
williambl <william@williambl.com>
2018-07-17 15:25:31 +0100

    Added an oblique clip plane Doesn't quite work right yet

--- unity-easy-portals/Assets/Scripts/PortalMoveCamera.cs
+++ unity-easy-portals/Assets/Scripts/PortalMoveCamera.cs
@@ -18,12 +18,26 @@ public class PortalMoveCamera : MonoBehaviour {
         //Modified from https://github.com/sclark39/Portal-In-Unity/blob/master/Portal/Assets/PortalCamera.cs
         Camera mainCamera = Camera.main;
         portalCamera.fieldOfView = mainCamera.fieldOfView;
-        portalCamera.nearClipPlane = Vector3.Distance(transform.position, portal.position);
-
+        
         Vector3 playerOffsetFromPortal = otherPortal.InverseTransformPoint(mainCamera.transform.position);
         transform.position = portal.TransformPoint(playerOffsetFromPortal);
 
         Quaternion relative = Quaternion.Inverse(otherPortal.transform.rotation) * mainCamera.transform.rotation;
         transform.rotation = portal.transform.rotation * relative;
+        
+        Plane portalPlane = new Plane(portal.forward, portal.position);
+        //Create an oblique clip plane in the same position and rotation as the portal
+        Vector4 clipPlaneWorld = new Vector4(
+                    -portal.forward.x,
+                    -portal.forward.y,
+                    -portal.forward.z,
+                    Mathf.Abs(portalPlane.GetDistanceToPoint(transform.position)));
+
+        Debug.Log(Mathf.Abs(portalPlane.GetDistanceToPoint(transform.position)));
+        //Put it into Camera Space
+        Vector4 clipPlaneCamera = Matrix4x4.Transpose(portalCamera.cameraToWorldMatrix) * clipPlaneWorld;
+        //Apply it to the projection matrix
+        portalCamera.projectionMatrix = portalCamera.CalculateObliqueMatrix(clipPlaneCamera);
+
     }
 }
